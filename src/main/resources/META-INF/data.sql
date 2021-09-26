034fdeffc613c0ee1032a4fed05743944f2071d1
JorgeAlca <943474556a@gmail.com>
2021-09-26 04:21:42 -0500

    Data de las tablas
    Archivo data.sql cargado con información (Tabla ambiente, Tabla distrito)

---
+++ src/main/resources/META-INF/data.sql
@@ -0,0 +1,55 @@
+INSERT INTO ambiente (nombre,precio) VALUES ('Sala',25);
+INSERT INTO ambiente (nombre,precio) VALUES ('Comedor',20);
+INSERT INTO ambiente (nombre,precio) VALUES ('Cocina',30);
+INSERT INTO ambiente (nombre,precio) VALUES ('Jardin',30);
+INSERT INTO ambiente (nombre,precio) VALUES ('Terraza',20);
+INSERT INTO ambiente (nombre,precio) VALUES ('Cuarto',15);
+INSERT INTO ambiente (nombre,precio) VALUES ('Baño',20);
+
+
+
+INSERT INTO distrito (nombre) VALUES ('Ancon');
+INSERT INTO distrito (nombre) VALUES ('Ate');
+INSERT INTO distrito (nombre) VALUES ('Barranco');
+INSERT INTO distrito (nombre) VALUES ('Breña');
+INSERT INTO distrito (nombre) VALUES ('Carabayllo');
+INSERT INTO distrito (nombre) VALUES ('Chaclacayo');
+INSERT INTO distrito (nombre) VALUES ('Chorrillos');
+INSERT INTO distrito (nombre) VALUES ('Cieneguilla');
+INSERT INTO distrito (nombre) VALUES ('Comas');
+INSERT INTO distrito (nombre) VALUES ('El Agustino');
+INSERT INTO distrito (nombre) VALUES ('Independencia');
+INSERT INTO distrito (nombre) VALUES ('Jesús María');
+INSERT INTO distrito (nombre) VALUES ('La Molina');
+INSERT INTO distrito (nombre) VALUES ('La Victoria');
+INSERT INTO distrito (nombre) VALUES ('Lima');
+INSERT INTO distrito (nombre) VALUES ('Lince');
+INSERT INTO distrito (nombre) VALUES ('Los Olivos');
+INSERT INTO distrito (nombre) VALUES ('Lurigancho');
+INSERT INTO distrito (nombre) VALUES ('Lurín');
+INSERT INTO distrito (nombre) VALUES ('Magdalena del Mar');
+INSERT INTO distrito (nombre) VALUES ('Miraflores');
+INSERT INTO distrito (nombre) VALUES ('Pachacamac');
+INSERT INTO distrito (nombre) VALUES ('Pucusana');
+INSERT INTO distrito (nombre) VALUES ('Pueblo Libre');
+INSERT INTO distrito (nombre) VALUES ('Puente Piedra');
+INSERT INTO distrito (nombre) VALUES ('Punta Hermosa');
+INSERT INTO distrito (nombre) VALUES ('Punta Negra');
+INSERT INTO distrito (nombre) VALUES ('Rimac');
+INSERT INTO distrito (nombre) VALUES ('San Bartolo');
+INSERT INTO distrito (nombre) VALUES ('San Borja');
+INSERT INTO distrito (nombre) VALUES ('San Isidro');
+INSERT INTO distrito (nombre) VALUES ('San Juan de Lurigancho');
+INSERT INTO distrito (nombre) VALUES ('San Juan de Miraflores');
+INSERT INTO distrito (nombre) VALUES ('San Luis');
+INSERT INTO distrito (nombre) VALUES ('San Martín de Porres');
+INSERT INTO distrito (nombre) VALUES ('San Miguel');
+INSERT INTO distrito (nombre) VALUES ('Santa Anita');
+INSERT INTO distrito (nombre) VALUES ('Santa María del Mar');
+INSERT INTO distrito (nombre) VALUES ('Santa Rosa');
+INSERT INTO distrito (nombre) VALUES ('Santiago de Surco');
+INSERT INTO distrito (nombre) VALUES ('Surquillo');
+INSERT INTO distrito (nombre) VALUES ('Villa El Salvador');
+INSERT INTO distrito (nombre) VALUES ('Villa María del Triunfo');
+
+
