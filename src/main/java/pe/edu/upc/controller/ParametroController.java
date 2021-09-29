5a94bcd1b4659a108d971c2923b83ebec743905d
FatimaGalindo <fatima_gali.vilca@hotmail.com>
2021-09-29 02:10:28 -0500

    Fix: Modificar Parametro controller

--- src/main/java/pe/edu/upc/controller/ParametroController.java
+++ src/main/java/pe/edu/upc/controller/ParametroController.java
@@ -6,105 +6,91 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.primefaces.event.SelectEvent;
 
 import pe.edu.upc.util.Message;
 import pe.edu.upc.entity.Parametro;
 import pe.edu.upc.serviceimpl.ParametroServiceImpl;
 
 @Named
-@RequestScoped
-public class ParametroController  implements Serializable {
+@SessionScoped
+public class ParametroController implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Inject
 	private ParametroServiceImpl parameterService;
-	
+
 	private Parametro parameter;
 	private List<Parametro> listaParametro;
-	
-	
+
 	@PostConstruct
-	public void init() {		
+	public void init() {
 		parameter = new Parametro();
 		this.listaParametro = new ArrayList<Parametro>();
 		getAllParameters();
 	}
-	
+
 	public void resetForm() {
 		this.parameter = new Parametro();
 	}
-	
-	public void obtenerParametro()
-	{
+
+	public void obtenerParametro() {
 		parameter.getId_parametro();
 	}
 
-	public String listadoParametros()
-	{
+	public String listadoParametros() {
 		System.out.println("hola");
 		return "/parameters/listParameters";
 	}
-	
-	public String editParameter (Parametro parameter) {
-		this.setParametro(parameter);
-		
-		System.out.println("Entrando a metodo editar");
-		System.out.println("Id: " + parameter.getId_parametro());
-		System.out.println("Nombre: " + parameter.getNombre());
-		System.out.println("Valor: " + parameter.getValor());
-		String view = "";		
+
+	public String editParameter(Parametro parameter) {
+		String view = "";
 		try {
-			if(this.getParametro() != null) {
-				
-				view = "/parameters/modifyParameter";
-			}
-			else {
-				Message.messageError("Error");
-			}
-		}
-		catch(Exception ex) {
-			Message.messageError("Error: " + ex.getMessage());
+
+			this.parameter = parameter;
+			view = "/parameters/modifyParameter?faces-redirect=true";
+
+			Message.messageError("Debe seleccionar un parametro");
+
+		} catch (Exception e) {
+			Message.messageError("Error en parametro " + e.getMessage());
 		}
 		return view;
 	}
-	
+
 	public void getAllParameters() {
-		try
-		{
+		try {
 			listaParametro = parameterService.findAll();
-		}
-		catch(Exception e)
-		{
+		} catch (Exception e) {
 			Message.messageError("Error");
 		}
 	}
-	
-	
+
 	public String guardarParametro() {
-		System.out.println("guardar");
-		System.out.println("valor"+ parameter.getValor());
-		System.out.println("id" + this.parameter.getId_parametro());
+
 		String view = "";
-		try{
-			if(parameter.getId_parametro() >= 1) {
+		try {
+			if (parameter.getId_parametro() != null) {
 				parameterService.editarParametro(parameter);
-				Message.messageInfo("Actualizado");
+				Message.messageInfo("Registro actualizado correctamente");
+			} else {
+
+				parameterService.insertar(parameter);
+				Message.messageInfo("Registro actualizado correctamente");
 			}
-		else{
-			parameterService.editarParametro(parameter);
-		}
-		this.getAllParameters();
-		resetForm();
-		view = "/parameters/listParameters";
-		}
-		catch(Exception e) {
+			this.getAllParameters();
+			resetForm();
+			view = "/parameters/listParameters?faces-redirect=true";
+		} catch (Exception e) {
+			// TODO: handle exception
 		}
 		return view;
 	}
-	
+
 	public List<Parametro> getListaParametro() {
 		return listaParametro;
 	}
@@ -112,11 +98,11 @@ public class ParametroController  implements Serializable {
 	public void setListaParametro(List<Parametro> listaParametro) {
 		this.listaParametro = listaParametro;
 	}
-	
+
 	public Parametro getParametro() {
 		return parameter;
 	}
-	
+
 	public void setParametro(Parametro parameter) {
 		this.parameter = parameter;
 	}
